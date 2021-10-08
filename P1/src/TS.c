228188ddcaf711c4666657eb21da5d9e86304457
David Carracedo <davidcm62@gmail.com>
2021-10-08 16:29:42 +0200

    remove old keywords

--- P1/src/TS.c
+++ P1/src/TS.c
@@ -49,17 +49,6 @@ TS* crearTS(){
         {"yield", _YIELD}
     };
 
-    //max 15 char
-    // char keywordss[][15] = {
-    //     "False",    "await",    "else",     "import",   "pass",
-    //     "None",     "break",    "except",   "in",       "raise",
-    //     "True",     "class",    "finally",  "is",       "return",
-    //     "and",      "continue", "for",      "lambda",   "try",
-    //     "as",       "def",      "from",     "nonlocal", "while",
-    //     "assert",   "del",      "global",   "not",      "with",
-    //     "async",    "elif",     "if",       "or",       "yield"
-    // };
-
     for(size_t i=0; i<sizeof(keywords)/sizeof(Keyword); i++){
         insertarHashMap(ts, keywords[i].lexema,keywords[i].compLexico);
     }
