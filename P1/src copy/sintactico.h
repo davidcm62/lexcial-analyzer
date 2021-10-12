a97e2a503a222593dfdff47bf33cf8bd299263f8
David Carracedo <davidcm62@gmail.com>
2021-10-12 17:13:31 +0200

    remove src copy

--- P1/src copy/sintactico.h
+++
@@ -1,9 +0,0 @@
-#ifndef SINTACTICO_H
-#define SINTACTICO_H
-
-#include "TS.h"
-#include "sisEntrada.h"
-
-void iniciarAnalisisSintactico(TS *tablaSimbolos,  SistemaEntrada *sistemaEntrada);
-
-#endif /* SINTACTICO_H */
