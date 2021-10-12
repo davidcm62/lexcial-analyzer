8277dfe8662363014d64a54885350fa799cfd7ed
David Carracedo <davidcm62@gmail.com>
2021-10-12 17:17:17 +0200

    test color output

--- P1/src/main.c
+++ P1/src/main.c
@@ -3,6 +3,22 @@
 #include "sisEntrada.h"
 #include "sintactico.h"
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void printRed(const char *msg){
+    printf(ANSI_COLOR_RED"%s"ANSI_COLOR_RESET"\n",msg);
+}
+
+void printBlue(const char *msg){
+    printf(ANSI_COLOR_CYAN"%s"ANSI_COLOR_RESET"\n",msg);
+}
+
 int main(int argc, char const **argv){
     if(argc < 2){
         //TODO: imprimir error
@@ -20,6 +36,10 @@ int main(int argc, char const **argv){
 
     liberarSistemaEntrada(sistemaEntrada);
     liberarTS(ts);
+
+    printRed("Hola en rojo");
+    printBlue("Hola en azul");
+    printf("Hola normal\n");
 
     return 0;
 }
