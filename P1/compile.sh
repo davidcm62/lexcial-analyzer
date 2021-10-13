f691996f7ea4386058c9a6058939b7b1d16c413e
David Carracedo <davidcm62@gmail.com>
2021-10-13 17:14:57 +0200

    remove py files

--- P1/compile.sh
+++ P1/compile.sh
@@ -14,7 +14,5 @@ echo "Executing..."
 echo ""
 
 # ./compilador ./python/wilcoxon.py
-# ./compilador ./python/test1.py
-# ./compilador ./python/test2.py
-# ./compilador ./python/test3.py
-./compilador ./python/test4.py
+./compilador ./python/wilcoxon-bad.py
+# ./compilador ./python/test4.py
--- P1/todo.md
+++
@@ -1,2 +0,0 @@
-[] prueba de estres a TS (15.000 lexemas)
-[] variable linea actual, offset desde o principio da linea en sisentrada para poder saber donde esta o error (tamen posicion dentro do ficheiro (podo sabelo co cursor do archivo))
